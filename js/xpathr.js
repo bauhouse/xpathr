9a59164687d71ce3bbe19d75ae8740e08b611aa7
Stephen Bau <stephen@domain7.com>
2012-11-11 11:01:34 -0800

    Grab the first bit of relevant code from the jsbin.js file and copy into xpathr.js

---
+++ js/xpathr.js
@@ -0,0 +1,14 @@
+jsbin.getURL = function () {
+  var url = jsbin.root,
+      state = jsbin.state;
+
+  if (state.code) {
+    url += '/' + state.code;
+
+    if (state.revision) { //} && state.revision !== 1) {
+      url += '/' + state.revision;
+    }
+  }
+  return url;
+};
+
